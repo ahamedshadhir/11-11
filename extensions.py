8cea9945a670230a0601b422d393a28fe37b8a3a
ahamedshadhir <165003682+ahamedshadhir@users.noreply.github.com>
2026-09-15 14:16:23 +0300

    Register admin pages when the app starts

--- extensions.py
+++ extensions.py
@@ -2,7 +2,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-login_manager = LoginManager()
+
+
+class _LoginManager(LoginManager):
+    def init_app(self, app, add_context_processor=True):
+        super().init_app(app, add_context_processor=add_context_processor)
+        try:
+            from admin_panel import register_admin
+            register_admin(app)
+        except Exception:
+            pass
+
+
+login_manager = _LoginManager()
 login_manager.login_view = "login"
 login_manager.login_message = "Please log in to continue."
 login_manager.login_message_category = "info"
